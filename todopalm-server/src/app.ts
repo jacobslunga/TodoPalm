d25fd47961be86730947c24c3fc57cf2b08ea787
Jacob Slunga <jacobslunga@Jacobs-MacBook-Air.local>
2023-11-09 21:12:21 +0100

    tried to start testing for server but not finished yet, todo: create a prisma mock

--- todopalm-server/src/app.ts
+++ todopalm-server/src/app.ts
@@ -4,7 +4,6 @@ import { authenticateJwtRequestToken } from "./api/v1/middleware/jwt";
 import v1Router from "./api/v1/routes";
 
 const app = express();
-const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
@@ -12,9 +11,5 @@ app.use(express.urlencoded({ extended: true }));
 app.use(authenticateJwtRequestToken());
 
 app.use("/api/v1", v1Router);
-
-app.listen(port, () => {
-  console.log(`Server listening on port ${port} 🚀`);
-});
 
 export default app;
--- todopalm-server/src/prismaClient.ts
+++ todopalm-server/src/prismaClient.ts
@@ -1,3 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url:
+        process.env.NODE_ENV === "test"
+          ? process.env.DATABASE_URL_DEV
+          : process.env.DATABASE_URL_DEV,
+    },
+  },
+});
+
+export { prisma };
